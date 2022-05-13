6242f96ca68a519d8f1275ae40aaffe6cc57a8f8
joanna-tang <101609196+joanna-tang@users.noreply.github.com>
2022-05-12 23:14:07 -0700

    Add files via upload

---
+++ resources.tf
@@ -0,0 +1,42 @@
+
+variable "awsprofile" {
+  type = string
+  default = "jtang-nclouds"
+}
+########################################################################
+#  Provider
+########################################################################
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  #backend "s3" {
+  # bucket = "jtang-nclouds-academy-2022"
+  # key    = "hw2.tfstate"
+  # region = "us-west-1"
+  #}
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "${var.awsprofile}"
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "launchconfig" {
+  source = "./modules/launchconfig"
+  prefix = module.vpc.prefix
+}
+
+module "autoscaling" {
+  source = "./modules/autoscaling"
+  prefix = module.vpc.prefix
+  launch_config_name = module.launchconfig.launch_config_name
+  subnet_ids = [module.vpc.privatesubnet1_id, module.vpc.privatesubnet2_id, module.vpc.privatesubnet3_id]
+}
